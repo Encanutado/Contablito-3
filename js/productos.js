4ba1be6a15fcd08ae19c0f63b12f536468e60b37
reptilcuantico99 <88388887+reptilcuantico99@users.noreply.github.com>
2021-10-05 08:32:20 -0300

    5/10
    Actualización: prueba de agregar imagenes con titulo

--- js/productos.js
+++ js/productos.js
@@ -1,27 +1,29 @@
-const formularioAgregar = document.getElementById("agregarProducto");
+
+  
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  const formularioAgregar = document.getElementById("agregarProducto");
 
 
-
-formularioAgregar.addEventListener("submit", function (evento) {
+  formularioAgregar.addEventListener("submit", function (evento) {
     /*Evita que la pagina se recargue automaticamente.*/
     evento.preventDefault();
   //Variables utilizadas.
-  var nombreProducto = document.getElementById("Nombre").value;
-  var descripcionProducto = document.getElementById("descripcionProducto").value;
-
-  agregarHtml += `
+  //var nombreProducto = document.getElementById("Nombre").value;
+  //var descripcionProducto = document.getElementById("descripcionProducto").value;
+  let agregarImagen= "";
+  agregarImagen += `
   <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <img class="card-img-top" id="imgItem" src="imgProductos/patricia.jpg" alt="Card image cap">
+  <div class="text-center">
+    <h4 class="nombreItem">cerveza</h4>
   </div>
 </div>
-  
-    `
-            
-        document.getElementById("principal").innerHTML = agregarHtml;
+ `         
+        document.getElementById("principal").innerHTML += agregarImagen;
   
   
   
   
   });
+});
